384c1aaf058904d727d6fddb81904b1f5802d13b
Badal Prasad Singh <badalstar1806@gmail.com>
2024-06-13 07:17:40 +0530

    only individual projects

--- 11 June/pipeline1.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis")
-res = classifier([
-    "I have suffering from Cancer",
-    "I am feeling very happy"
-])
-print(res)
